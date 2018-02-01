98a70eeaa0c44c4ecbb2065901388382e378229a
Simon Fong <simonfong6@gmail.com>
2018-02-01 13:01:30 -0800

    Implemented text database, and updating map

---
+++ server.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, send_from_directory, jsonify
+import json
+
+
+DATA_FILE = 'data.json'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    return send_from_directory('.', 'index.html')
+
+@app.route('/markers')
+def get_markers():
+    with open(DATA_FILE, 'r') as f:
+        data = json.load(f)
+    
+    return jsonify(data)
+    
+@app.route('/markers/update', methods=['GET','POST'])
+def update_markers():
+    
+    marker = request.json
+    
+    label = marker['label']
+
+    with open(DATA_FILE, 'r+') as f:
+        data = json.load(f)
+        
+        updated = False
+        
+        for index,marker_db in enumerate(data['markers']):
+            if(marker_db['label'] == label):
+                data['markers'][index]['position'] = marker['position']
+                print("FOUND")
+                updated = True
+        
+        if(not updated):
+            data['markers'].append(marker)
+            print("NOT FOUND")
+
+        
+        f.seek(0)
+        
+        json.dump(data, f, indent=4)
+        
+        f.truncate()
+        
+    return "Success"
+	
+if(__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=3454)
